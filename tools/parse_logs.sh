c658fcb3abd3b49b6a614df6d7ac4c5585017c07
Lei Song <lsong@clarku.edu>
2021-05-24 00:59:11 -0400

    Modify the tools

--- tools/parse_logs.sh
+++ tools/parse_logs.sh
@@ -5,19 +5,25 @@
 # Get input parameters
 DNS=$1
 if [ -z "$DNS" ]; then
-    echo "`date`: Usage: $0 <Public_IPv4_DNS> <experiment_name> <download_dir>"
+    echo "`date`: Usage: $0 <Public_IPv4_DNS> <experiment_name> <download_dir> <sleep_sec>"
     exit 1
 fi
 ENAME=$2
 if [ -z "$ENAME" ]; then
-    echo "`date`: Usage: $0 <Public_IPv4_DNS> <experiment_name> <download_dir>"
+    echo "`date`: Usage: $0 <Public_IPv4_DNS> <experiment_name> <download_dir> <sleep_sec>"
     exit 1
 fi
 DIR=$3
 if [ -z "$DIR" ]; then
-    echo "`date`: Usage: $0 <Public_IPv4_DNS> <experiment_name> <download_dir>"
+    echo "`date`: Usage: $0 <Public_IPv4_DNS> <experiment_name> <download_dir> <sleep_sec>"
+    exit 1
+fi
+STIME=$4
+if [ -z "$STIME" ]; then
+    echo "`date`: Usage: $0 <Public_IPv4_DNS> <experiment_name> <download_dir> <sleep_sec>"
     exit 1
 fi
 
+
 # Download files in loop
-while true; do scp -r ubuntu@$DNS:~/hrlcm/results/dl/$ENAME $DIR; sleep 2000; done
+while true; do scp -r ubuntu@$DNS:~/hrlcm/results/dl/$ENAME $DIR; sleep $STIME; done
