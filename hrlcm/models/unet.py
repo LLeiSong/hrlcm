16280cd66b7ef6a1afce1abe30f09fe4425888ea
LLeiSong <pinot.sung@gmail.com>
2021-08-12 13:37:49 -0400

    Try replicate pad

--- hrlcm/models/unet.py
+++ hrlcm/models/unet.py
@@ -7,7 +7,7 @@ from models.utils.unet_parts import *
 
 
 class UNet(nn.Module):
-    def __init__(self, n_channels, n_classes, bilinear=True):
+    def __init__(self, n_channels, n_classes, bilinear=False):
         super(UNet, self).__init__()
         self.n_channels = n_channels
         self.n_classes = n_classes
