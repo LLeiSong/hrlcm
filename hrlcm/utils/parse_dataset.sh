12106de3f298eab36cdb3ebfafb9919ef0d08bb6
Lei Song <lsong@clarku.edu>
2021-08-17 14:46:25 -0400

    Update parse dataset link

--- hrlcm/utils/parse_dataset.sh
+++ hrlcm/utils/parse_dataset.sh
@@ -9,7 +9,7 @@ if [ -z "$DIR" ]; then
 fi
 
 echo "Download dataset to $DIR/dl_data.zip"
-curl -o "$DIR/dl_data.zip" https://www.dropbox.com/s/u9ef5nb4a9htegc/dl_data.zip -L
+curl -o "$DIR/dl_data.zip" https://www.dropbox.com/s/ghlssjk7ojoj6ob/dl_train_valid.zip -L
 unzip $DIR/dl_data.zip -d $DIR
 rm -rf $DIR/dl_data.zip
 rm -rf $DIR/__MACOSX
