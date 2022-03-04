a7b933a56538b88c4792b6ed9a75d22d543317bc
Lei Song <lsong@clarku.edu>
2022-03-04 13:21:51 +0800

    Modify lr scheduler

--- hrlcm/lr_scheduler.py
+++ hrlcm/lr_scheduler.py
@@ -2,7 +2,7 @@ import torch
 import torch_optimizer as optim
 
 
-def get_compose_lr(model, epochs, min_lr=0.0001, max_lr=0.001, stage1=50, stage2=130, stage3=170):
+def get_compose_lr(model, epochs, min_lr=0.00001, max_lr=0.001, stage1=50, stage2=130, stage3=170):
     """Util function to customize a learning rate scheduler.
     Note: not a perfect function, user could customize this function based on needs.
         Args:
@@ -18,23 +18,23 @@ def get_compose_lr(model, epochs, min_lr=0.0001, max_lr=0.001, stage1=50, stage2
         """
 
     # Use an arbitrary optimizer
-    optimizer_lr = optim.AdaBound(model.parameters(), lr=max_lr, final_lr=0.0001, amsbound=True)
+    optimizer_lr = optim.AdaBound(model.parameters(), lr=max_lr, final_lr=0.01, amsbound=True)
     lrs = []
     for i in range(epochs):
         optimizer_lr.step()
         if i <= stage1:
             if i == stage1:
-                lr_scheduler_1 = torch.optim.lr_scheduler.CyclicLR(optimizer_lr, base_lr=max_lr - 0.000002,
-                                                                   max_lr=max_lr + 0.000002,
-                                                                   step_size_up=1, step_size_down=3,
+                lr_scheduler_1 = torch.optim.lr_scheduler.CyclicLR(optimizer_lr, base_lr=max_lr - 0.0004,
+                                                                   max_lr=max_lr + 0.0004,
+                                                                   step_size_up=1, step_size_down=5,
                                                                    gamma=0.97, cycle_momentum=False,
                                                                    mode='exp_range')
                 lr_scheduler_1.step()
         elif i <= stage2:
             lr_scheduler_1.step()
             if i == stage2:
-                lr_scheduler_2 = torch.optim.lr_scheduler.CyclicLR(optimizer_lr, base_lr=(max_lr - 0.000002) / 2,
-                                                                   max_lr=(max_lr + 0.000002) / 2,
+                lr_scheduler_2 = torch.optim.lr_scheduler.CyclicLR(optimizer_lr, base_lr=0.0001,
+                                                                   max_lr=0.0006,
                                                                    step_size_up=1, step_size_down=5,
                                                                    gamma=0.94, cycle_momentum=False,
                                                                    mode='exp_range')
